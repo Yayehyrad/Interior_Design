7bce22fa5ee50dcbf217b84079a32209c6401bbf
Yayehyrad <yayehyrad.sisay@gmail.com>
2024-05-21 22:58:19 +0300

    left with motions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import "./App.css";
+import Banner from "./components/Banner";
+import Collections from "./components/Collections";
+import Explore from "./components/Explore";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Intro from "./components/Intro";
+import Services from "./components/Services";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <div className=" text-red-800">hi working</div>
-    </>
+    <div className="w-full flex flex-col">
+      <Header />
+      <Services />
+      <Collections />
+      <Explore />
+      <Intro />
+      <Banner />
+      <Footer />
+    </div>
   );
-}
+};
 
 export default App;
