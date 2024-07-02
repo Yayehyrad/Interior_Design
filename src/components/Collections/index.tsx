6c7f1c38e89c428f62aae35e68a329ee55ba4b56
Yayehyrad <yayehyrad.sisay@gmail.com>
2024-07-02 22:08:42 +0300

    chore: Update npm dependencies and styles in Intro and Banner components

--- src/components/Collections/index.tsx
+++ src/components/Collections/index.tsx
@@ -4,56 +4,63 @@ import surreal from "../../assets/images/surreal.jpg";
 import bright from "../../assets/images/bright.jpg";
 
 const Collections = () => {
-	const collectionItems = [
-		{
-			title: "Classic",
-			image: classic,
-			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias omnis, ad modi expedita, velit aperiam dignissimos voluptas",
-		},
-		{
-			title: "Luxury",
-			image: luxury,
-			desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi quibusdam sint culpa aperiam, maiores velit, delectus iure.",
-		},
-		{
-			title: "Light , Bright",
-			image: bright,
-			desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi quibusdam sint culpa aperiam, maiores velit, delectus iure.",
-		},
-		{
-			title: "Surreal",
-			image: surreal,
-			desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi quibusdam sint culpa aperiam, maiores velit, delectus iure.",
-		},
-	];
-	return (
-		<div className="w-full lg:px-[150px] px-5 pb-8 flex lg:flex-row flex-col items-center justify-between gap-8">
-			{collectionItems.map((item, index: number) => (
-				<div
-					key={index}
-					className="w-full h-[600px] bg-center bg-cover relative p-6 cursor-pointer"
-					style={{ backgroundImage: `url(${item.image})` }}
-				>
-					<div className="w-full h-full absolute bg-black opacity-45 z-20 top-0 left-0"></div>
-					<div className="flex flex-col">
-						<div className="text-white font-semibold text-[20px] z-50">{item.title}</div>
-						<div className="text-white font-semibold text-[20px] z-50">Collection</div>
-					</div>
-					<div className="absolute bottom-6 z-50 flex flex-col gap-6">
-						<p className="text-gray-100 leading-6 text-[14px] lg:text-left text-center">
-							{item.desc}
-						</p>
-						<a
-							href="#"
-							className="text-white font-light text-[17px]"
-						>
-							Read More
-						</a>
-					</div>
-				</div>
-			))}
-		</div>
-	);
+  const collectionItems = [
+    {
+      title: "Classic",
+      image: classic,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias omnis, ad modi expedita, velit aperiam dignissimos voluptas",
+    },
+    {
+      title: "Luxury",
+      image: luxury,
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi quibusdam sint culpa aperiam, maiores velit, delectus iure.",
+    },
+    {
+      title: "Light , Bright",
+      image: bright,
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi quibusdam sint culpa aperiam, maiores velit, delectus iure.",
+    },
+    {
+      title: "Surreal",
+      image: surreal,
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi quibusdam sint culpa aperiam, maiores velit, delectus iure.",
+    },
+  ];
+  return (
+    <div
+      className="w-full lg:px-[150px] px-5 pb-8 flex lg:flex-row flex-col items-center justify-between gap-8 pt-10"
+      id="collections"
+    >
+      {collectionItems.map((item, index: number) => (
+        <div
+          key={index}
+          className="w-full h-[600px] bg-center bg-cover relative p-6 cursor-pointer group"
+          style={{ backgroundImage: `url(${item.image})` }}
+        >
+          <div className="w-full h-full absolute bg-black opacity-45 group-hover:opacity-65 z-20 top-0 left-0"></div>
+          <div className="flex flex-col">
+            <div className="text-white font-semibold text-[20px] z-50">
+              {item.title}
+            </div>
+            <div className="text-white font-semibold text-[20px] z-50">
+              Collection
+            </div>
+          </div>
+          <div className="absolute bottom-6 z-50 flex flex-col gap-6">
+            <p className="text-gray-100 leading-6 text-[14px] lg:text-left text-center  translate-y-12  group-hover:translate-y-0 transition-all duration-200">
+              {item.desc}
+            </p>
+            <a
+              href="#"
+              className="text-white font-light text-[17px]  translate-y-12  group-hover:translate-y-0 transition-all duration-200"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Collections;
--- src/components/Explore/index.tsx
+++ src/components/Explore/index.tsx
@@ -11,62 +11,35 @@ import nine from "../../assets/images/nine.jpg";
 import ten from "../../assets/images/ten.jpg";
 
 const Explore = () => {
-	return (
-		<div className="w-full lg:px-[150px] px-5 lg:py-10 py-8 flex flex-col items-center justify-center gap-5">
-			<div className="flex flex-col items-center">
-				<span className="text-[15px] text-[#425e85]">Explore</span>
-				<span className="font-semibold text-[30px] text-center">
-					Our Latest Interior Designs
-				</span>
-				<p className="mt-3 text-gray-500 text-center max-w-[500px]">
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur repellendus
-					error.
-				</p>
-			</div>
-			<div className="grid grid-cols-5 grid-rows-8 gap-4 w-full h-[800px] m-8">
-				<GridImage
-					image={one}
-					layout="col-span-2 row-span-3"
-				/>
-				<GridImage
-					image={two}
-					layout="col-span-2 row-span-3 col-start-3"
-				/>
-				<GridImage
-					image={three}
-					layout="row-span-3 col-start-5"
-				/>
-				<GridImage
-					image={four}
-					layout="row-span-2 row-start-4"
-				/>
-				<GridImage
-					image={five}
-					layout="col-span-2 row-span-2 row-start-4"
-				/>
-				<GridImage
-					image={six}
-					layout="col-span-2 row-span-2 col-start-4 row-start-4"
-				/>
-				<GridImage
-					image={seven}
-					layout="col-span-2 row-span-3 row-start-6"
-				/>
-				<GridImage
-					image={eight}
-					layout="row-span-3 col-start-3 row-start-6"
-				/>
-				<GridImage
-					image={nine}
-					layout="row-span-3 col-start-5 row-start-6"
-				/>
-				<GridImage
-					image={ten}
-					layout="row-span-3 col-start-4 row-start-6"
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full lg:px-[150px] px-5 lg:py-10 py-8 flex flex-col items-center justify-center gap-5">
+      <div className="flex flex-col items-center">
+        <span className="text-[15px] text-[#425e85]">Explore</span>
+        <span className="font-semibold text-[30px] text-center">
+          Our Latest Interior Designs
+        </span>
+        <p className="mt-3 text-gray-500 text-center max-w-[500px]">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+          repellendus error.
+        </p>
+      </div>
+      <div className="grid grid-cols-5 grid-rows-8 gap-4 w-full h-[800px] m-8">
+        <GridImage image={one} layout="col-span-2 row-span-3" />
+        <GridImage image={two} layout="col-span-2 row-span-3 col-start-3" />
+        <GridImage image={three} layout="row-span-3 col-start-5" />
+        <GridImage image={four} layout="row-span-2 row-start-4" />
+        <GridImage image={five} layout="col-span-2 row-span-2 row-start-4" />
+        <GridImage
+          image={six}
+          layout="col-span-2 row-span-2 col-start-4 row-start-4"
+        />
+        <GridImage image={seven} layout="col-span-2 row-span-3 row-start-6" />
+        <GridImage image={eight} layout="row-span-3 col-start-3 row-start-6" />
+        <GridImage image={nine} layout="row-span-3 col-start-5 row-start-6" />
+        <GridImage image={ten} layout="row-span-3 col-start-4 row-start-6" />
+      </div>
+    </div>
+  );
 };
 
 export default Explore;
